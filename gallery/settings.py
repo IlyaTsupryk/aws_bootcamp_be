139786d6e0c76d5ef27065ee3d6733b6d4cce6fc
Ilya Tsupryk <ilya_tsupryk@epam.com>
2023-05-25 07:57:49 +0200

    Fetch allowed hosts from env

--- gallery/settings.py
+++ gallery/settings.py
@@ -145,7 +145,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-]
+] + os.getenv("ALLOWED_HOSTS", "").split(",")
 
 CORS_ALLOW_HEADERS = [
     "Content-Type",
